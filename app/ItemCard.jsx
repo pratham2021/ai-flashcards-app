cec9354dd465090ce7b693b773e727ffbe47e4e9
pratham2021 <prathamhebbar2021@gmail.com>
2024-08-17 21:53:16 -0700

    update ItemCard

--- app/ItemCard.jsx
+++ app/ItemCard.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 const ItemCard = () => {
   return (
